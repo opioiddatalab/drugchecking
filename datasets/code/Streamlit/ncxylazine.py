22c0ce99421f4431164848af1919779e7876dc22
Drew Hackelman <drew.hackelman@gmail.com>
2023-06-22 15:38:28 +0000

    b92222f worked, trying to un-break the table

--- datasets/code/Streamlit/ncxylazine.py
+++ datasets/code/Streamlit/ncxylazine.py
@@ -59,11 +59,9 @@ latest = latest.strftime('%A %B %d, %Y')
 
 # Latest xylazine reports by county
 latestreport = dfxyl.groupby(by=["county"])
-for name, group in latestreport:
-    print(name)
-    print(group)
-mostrecent = latestreport['date_complete']
-# mostrecent.rename(columns={'date_complete': 'Most_Recent'}, inplace=True)
+lrdc = latestreport["date_complete"].apply(pd.DataFrame)
+mostrecent = lrdc.reset_index()
+mostrecent.rename(columns={'date_complete': 'Most_Recent'}, inplace=True)
 
 # Sensations Graph
 import altair as alt
