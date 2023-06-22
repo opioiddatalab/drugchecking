05219a1835c639c0212f5e8af3a379608928aa00
Drew Hackelman <drew.hackelman@gmail.com>
2023-06-22 15:15:40 +0000

    convert series groupby back to dataframe

--- datasets/code/Streamlit/ncxylazine.py
+++ datasets/code/Streamlit/ncxylazine.py
@@ -59,8 +59,8 @@ latest = latest.strftime('%A %B %d, %Y')
 
 # Latest xylazine reports by county
 latestreport = dfxyl.groupby(by=["county"])
-if isinstance(latestreport["date_complete"], pd.core.series.Series):
-    latestreport["date_complete"] = latestreport["date_complete"].dt.strftime('%B %d, %Y')
+latestreport["date_complete"].apply(pd.DataFrame)
+latestreport["date_complete"] = latestreport["date_complete"].dt.strftime('%B %d, %Y')
 mostrecent = latestreport['date_complete'].copy()
 mostrecent.rename(columns={'date_complete': 'Most_Recent'}, inplace=True)
 
