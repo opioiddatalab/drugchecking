409f4640aad2a8ed74a81af732b9f77cf08e69e6
Drew Hackelman <drew.hackelman@gmail.com>
2023-06-22 15:08:28 +0000

    check for series

--- datasets/code/Streamlit/ncxylazine.py
+++ datasets/code/Streamlit/ncxylazine.py
@@ -59,7 +59,8 @@ latest = latest.strftime('%A %B %d, %Y')
 
 # Latest xylazine reports by county
 latestreport = dfxyl.groupby(by=["county"])
-latestreport["date_complete"] = latestreport["date_complete"].dt.strftime('%A %B %d, %Y')
+if isinstance(latestreport["date_complete"], pd.core.series.Series):
+    latestreport["date_complete"] = latestreport["date_complete"].dt.strftime('%B %d, %Y')
 mostrecent = latestreport[['date_complete']].copy()
 mostrecent.rename(columns={'date_complete': 'Most_Recent'}, inplace=True)
 
