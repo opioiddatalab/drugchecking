3bee7d0a23f36308f676f162a9a71fac30c2bb88
Drew Hackelman <drew.hackelman@gmail.com>
2023-07-27 11:09:33 -0400

    messed up GA tag again

--- datasets/code/Streamlit/ncxylazine.py
+++ datasets/code/Streamlit/ncxylazine.py
@@ -20,7 +20,7 @@ st.markdown(
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-J6G2QFEL1Q');
+            gtag('config', 'G-B10YREJ18E');
         </script>
     """, unsafe_allow_html=True)
 
