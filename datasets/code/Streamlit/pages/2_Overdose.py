deae1fad262074848c09e305bb4093f8542bf40c
Drew Hackelman <drew.hackelman@gmail.com>
2023-08-01 13:46:58 -0400

    updating with linked sample ids in the overdose table

--- datasets/code/Streamlit/pages/2_Overdose.py
+++ datasets/code/Streamlit/pages/2_Overdose.py
@@ -3,15 +3,9 @@ local_css("datasets/code/Streamlit/style.css")
 import streamlit as st
 import pandas as pd
 import numpy as np
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
 import altair as alt
 from itertools import cycle
-
+import streamlit.components.v1 as components
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 
@@ -20,138 +14,118 @@ def get_nc_od_color():
     return pd.read_csv(url)
 nc_od_color = get_nc_od_color()
 nc_od_color_df = pd.DataFrame(nc_od_color)
-nc_od_color_df['date_collect'] = pd.to_datetime(nc_od_color_df['date_collect']).dt.strftime('%b %d, %Y')
-
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    modify = st.checkbox("Add filters")
-
-    if not modify:
-        return df
-
-    df = df.copy()
-
-    # Try to convert datetimes into format="dddd MMMM DD, YYYY",
-    for col in df.columns:
-      if col == 'date_collect':
-        try:
-          df[col] = pd.to_datetime(df[col], format='%Y-%m-%d').strftime('%M %d, %Y')
-        except Exception:
-          pass
-      if is_object_dtype(df[col]):
-        try:
-          df[col] = pd.to_datetime(df[col], format='%Y-%m-%d').strftime('%M %d, %Y')
-        except Exception:
-          pass
-
-      if is_datetime64_any_dtype(df[col]):
-        df[col] = pd.to_datetime(df[col], format='%Y-%m-%d ').strftime('%M %d, %Y')
-
-    modification_container = st.container()
-    with modification_container:
-        copy_df_columns = df.columns
-        copy_df_columns = copy_df_columns.drop('sampleid')
-        copy_df_columns = copy_df_columns.drop('date_collect')
-        to_filter_columns = st.multiselect("Filter dataframe on", copy_df_columns)
-        # remove pubchemcid from to_filter_columns
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            # Treat columns with < 10 unique values as categorical
-            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Select a date range",
-                    value=(
-                #  today's date minus 90 days
-                  pd.to_datetime("today") - pd.Timedelta(days=90),
-                  # make max the current date + the number of days remaining in the current month
-                  pd.to_datetime("today") + pd.offsets.MonthEnd(0)
-                    ),
-                    help="Use the date picker to select a date range. Defaults to previous 90 days. "
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
-
-    return df
-
+nc_od_color_df['date_collect'] = pd.to_datetime(nc_od_color_df['date_collect'])
 
 # create a title for the app
-st.title('Overdose Dashboard Title')
+st.title('Overdoses in NC')
 st.markdown("---")
 
 
 with st.container():
-  st.subheader("View Overdoses to filter by County or Drug Color")
+  st.subheader("View Overdoses by County", anchor="county", help=None)
 data_url = "https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv"
 
+LinkCellRenderer = JsCode('''
+  class LinkCellRenderer {
+      init(params) {
+          this.params = params;
+          this.eGui = document.createElement('div');
+          this.eGui.innerHTML = `
+          <span>
+              <a id='click-button'
+                  class='btn-simple'
+                  href='https://streetsafe.supply/results/p/${this.params.getValue()}'
+                  target='_blank'
+                  style='color: ${this.params.color};}'>${this.params.getValue()}</a>
+          </span>
+        `;
 
-cellRenderer=JsCode('''
-                    function(params) {
-                      return "<a href='www.streetsafe.supply/results/p/'" + params.value + " target='_blank'>"+ params.value + "</a>"
-                    }
-                  ''')
+          this.eButton = this.eGui.querySelector('#click-button');
 
-#Infer basic colDefs from dataframe types
+          this.btnClickedHandler = this.btnClickedHandler.bind(this);
+          this.eButton.addEventListener('click', this.btnClickedHandler);
+
+      }
+
+      getGui() {
+          return this.eGui;
+      }
+
+      refresh() {
+          return true;
+      }
+
+      destroy() {
+          if (this.eButton) {
+              this.eGui.removeEventListener('click', this.btnClickedHandler);
+          }
+      }
+
+      btnClickedHandler(event) {
+
+                  this.refreshTable('clicked');
+          }
+
+      refreshTable(value) {
+          this.params.setValue(value);
+      }
+  };
+''')
+
+# Infer basic colDefs from dataframe types
 gb = GridOptionsBuilder.from_dataframe(nc_od_color_df)
 
 #customize gridOptions
-gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True)
 gb.configure_column("date_collect", type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='yyyy-MM-dd', pivot=True)
 
-# gb.configure_column("sampleid", cellStyle=cellRenderer)
-
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
-
 gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=25)
-return_mode_value = 'Filtered'
 fit_columns_on_grid_load = True
 gb.configure_grid_options(domLayout='normal')
 gridOptions = gb.build()
 
 #Display the grid
 
+gridOptions['columnDefs'] = [
+    #  rename the sampleid column to Sample ID
+    {
+    "field": "sampleid",
+    "headerName": "ID",
+    "cellRenderer": LinkCellRenderer,
+    "cellRendererParams": {
+      "color": "blue",
+      "data": "sampleid"
+    },
+    "maxWidth": 90,
+  },
+  {
+    "field": "date_collect",
+    "headerName": "Date Collected",
+    "type": ["dateColumnFilter","customDateTimeFormat"],
+    "custom_format_string": "MM-dd-yyyy",
+    "pivot": True,
+    "maxWidth": 120,
+  },
+  {
+    "field": "county",
+    "headerName": "County"
+  },
+  {
+    "field": "texture",
+    "headerName": "Textures"
+  }
+]
 grid_response = AgGrid(
     nc_od_color_df,
     gridOptions=gridOptions,
     width='100%',
-    data_return_mode=return_mode_value,
-    update_mode='GRID_CHANGED',
     fit_columns_on_grid_load=fit_columns_on_grid_load,
     allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
     enable_enterprise_modules=False
     )
-
 df = grid_response['data']
 selected = grid_response['selected_rows']
 selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
-
-
-st.dataframe(filter_dataframe(nc_od_color_df), use_container_width=True)
 
 
 outerCol1, outerCol2 = st.columns(2)
--- datasets/code/Streamlit/pages/5_Drug_Market.py
+++ datasets/code/Streamlit/pages/5_Drug_Market.py
@@ -4,12 +4,6 @@ import streamlit as st
 import pandas as pd
 import json
 import random
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -40,7 +34,7 @@ def get_nc_ds_substances():
 nc_market_substances = get_nc_ds_substances()
 nc_market_substances = pd.DataFrame(nc_market_substances)
 # map over the latest_dected col and convert to human readable date
-nc_market_substances['latest_detected'] = pd.to_datetime(nc_market_substances['latest_detected'])
+nc_market_substances['latest_detected'] = pd.to_datetime(nc_market_substances['latest_detected']).dt.strftime('%b %d, %Y')
 # map over the pubchemcid col and if there is no value, add "none" to the cel
 nc_market_substances['pubchemcid'] = nc_market_substances['pubchemcid'].apply(lambda x: x if x != 'nan' else 'none')
 # make it categorical, otherwise the commas get inserted and it treats them as floats
@@ -134,100 +128,7 @@ fig, ax = plt.subplots()
 sns.heatmap(nc_market_substances_top_10.corr(), annot=True,fmt=".2f",mask=np.triu(np.ones_like(nc_market_substances_top_10.corr(),dtype=bool)))
 st.write(fig)
 
-# def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-#     """
-#     Adds a UI on top of a dataframe to let viewers filter columns
 
-#     Args:
-#         df (pd.DataFrame): Original dataframe
-
-#     Returns:
-#         pd.DataFrame: Filtered dataframe
-#     """
-#     modify = st.checkbox("Add filters")
-
-#     if not modify:
-#         return df
-
-#     df = df.copy()
-
-#     # Try to convert datetimes into format="dddd MMMM DD, YYYY",
-
-#     for col in df.columns:
-#       if col == 'pubchemcid':
-#          pass
-#       if col == 'latest_detected':
-#         try:
-#           df[col] = pd.to_datetime(df[col], format='%Y-%m-%d')
-#         except Exception:
-#           pass
-#       if is_object_dtype(df[col]):
-#         try:
-#           df[col] = pd.to_datetime(df[col], format='%Y-%m-%d')
-#         except Exception:
-#           pass
-
-#       if is_datetime64_any_dtype(df[col]):
-#         df[col] = pd.to_datetime(df[col], format='%Y-%m-%d ')
-
-#     modification_container = st.container()
-#     with modification_container:
-#         # @Nab - having an issue here because of the empty pubchemcid col values
-#         # make a copy of df.columns but remove pubchemcid
-#         copy_df_columns = df.columns
-#         copy_df_columns = copy_df_columns.drop('pubchemcid')
-#         to_filter_columns = st.multiselect("Filter dataframe on", copy_df_columns)
-#         # remove pubchemcid from to_filter_columns
-#         for column in to_filter_columns:
-#             left, right = st.columns((1, 20))
-#             # Treat columns with < 10 unique values as categorical
-#             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-#                 user_cat_input = right.multiselect(
-#                     f"Values for {column}",
-#                     df[column].unique(),
-#                     default=list(df[column].unique()),
-#                 )
-#                 df = df[df[column].isin(user_cat_input)]
-#             elif is_numeric_dtype(df[column]):
-#                 _min = float(df[column].min())
-#                 _max = float(df[column].max())
-#                 step = (_max - _min) / 100
-#                 user_num_input = right.slider(
-#                     f"Values for {column}",
-#                     min_value=_min,
-#                     max_value=_max,
-#                     value=(_min, _max),
-#                     step=step,
-#                 )
-#                 df = df[df[column].between(*user_num_input)]
-#             elif is_datetime64_any_dtype(df[column]):
-#                 user_date_input = right.date_input(
-#                     f"Select a date range",
-#                     value=(
-#                 #  today's date minus 90 days
-#                   pd.to_datetime("today") - pd.Timedelta(days=90),
-#                   # make max the current date + the number of days remaining in the current month
-#                   pd.to_datetime("today") + pd.offsets.MonthEnd(0)
-#                     ),
-#                     help="Use the date picker to select a date range. Defaults to previous 90 days. "
-#                 )
-#                 if len(user_date_input) == 2:
-#                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
-#                     start_date, end_date = user_date_input
-#                     df = df.loc[df[column].between(start_date, end_date)]
-#             else:
-#                 user_text_input = right.text_input(
-#                     f"Substring or regex in {column}",
-#                 )
-#                 if user_text_input:
-#                     df = df[df[column].astype(str).str.contains(user_text_input)]
-
-#     return df
-# data_url = "https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv"
-
-# st.dataframe(filter_dataframe(nc_market_substances))
-
-# st.dataframe(nc_market_substances_df.style.highlight_max(axis=0), height=300, width=800)
 st.markdown("---")
 st.markdown("## How pure is the NC drug supply?")
 st.markdown("**the number of substances detected is a measurement of how contaminated the drug supply is*")
